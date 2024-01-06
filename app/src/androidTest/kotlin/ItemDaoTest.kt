a76d656f29db170bbad668fde5dc612375468aa3
LEE MUTAI <71839051+leemutai@users.noreply.github.com>
2024-01-06 13:03:43 +0300

    implement sell item

--- app/src/androidTest/kotlin/ItemDaoTest.kt
+++ app/src/androidTest/kotlin/ItemDaoTest.kt
@@ -15,13 +15,14 @@ import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertEquals
 import org.junit.Test
-
+import org.junit.Assert.assertTrue
 @RunWith(AndroidJUnit4::class)
 class ItemDaoTest {
     private lateinit var itemDao: ItemDao
     private lateinit var inventoryDatabase: InventoryDatabase
     private var item1 = Item(1, "Apples", 10.0, 20)
     private var item2 = Item(2, "Bananas", 15.0, 97)
+
 
     @Before
     fun createDb() {
@@ -44,8 +45,13 @@ class ItemDaoTest {
     @Throws(Exception::class)
     fun daoInsert_insertsItemIntoDB() = runBlocking {
         addOneItemToDb()
+        itemDao.update(Item(1, "Apples", 15.0, 25))
+        itemDao.update(Item(2, "Bananas", 5.0, 50))
+
         val allItems = itemDao.getAllItems().first()
-        assertEquals(allItems[0], item1)
+        assertEquals(allItems[0], Item(1, "Apples", 15.0, 25))
+        assertEquals(allItems[1], Item(2, "Bananas", 5.0, 50))
+
     }
 
     @Test
@@ -56,6 +62,30 @@ class ItemDaoTest {
         assertEquals(allItems[0], item1)
         assertEquals(allItems[1], item2)
     }
+    @Test
+    @Throws(Exception::class)
+    fun  daoUpdateItems_updatesItemsInDB() = runBlocking {
+        addTwoItemsToDb()
+    }
+
+
+    @Test
+    @Throws(Exception::class)
+    fun daoDeleteItems_deletesAllItemsFromDB() = runBlocking {
+        addTwoItemsToDb()
+        itemDao.delete(item1)
+        itemDao.delete(item2)
+        val allItems = itemDao.getAllItems().first()
+        assertTrue(allItems.isEmpty())
+    }
+    @Test
+    @Throws(Exception::class)
+    fun daoGetItem_returnsItemFromDB() = runBlocking {
+        addOneItemToDb()
+        val item = itemDao.getItem(1)
+        assertEquals(item.first(), item1)
+    }
+
     private suspend fun addOneItemToDb() {
         itemDao.insert(item1)
     }
@@ -64,4 +94,6 @@ class ItemDaoTest {
         itemDao.insert(item1)
         itemDao.insert(item2)
     }
+
 }
+
